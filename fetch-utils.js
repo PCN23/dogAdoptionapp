98b372e35f63fc436ae3e6eaec339ce71af8743e
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-11 11:32:03 -0700

    added first get dog export

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,17 +1,22 @@
-const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getDogs() {
     // from the dogs table, select all items
+    const response = await client.from('dogs').select();
+    console.log(response);
+    return response.data;
 
     // and return the response
-    return response.data;    
+   
 }
 
 export async function getDog(id) {
     // from the dogs table, select a single dog who has the matching id
+const response = await client.from('dogs').select()
+
 
     // and return the response
     return response.data;    
